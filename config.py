807885c51aedb3ae4818561d332abe8309a4bf5e
UmurerwaDivine <divinekagame@gmail.com>
2019-03-05 08:33:56 +0200

    heroku

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
 
     QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://divine:dDivine123@@localhost/blogs'
+   
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     #  email configurations
     MAIL_SERVER = 'smtp.googlemail.com'
@@ -16,12 +16,12 @@ class Config:
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://divine:dDivine123@@localhost/blogs'
 
 
 class DevConfig(Config):
+   
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://divine:dDivine123@@localhost/blogs'
-    
 
     DEBUG = True
 
